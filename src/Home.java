9968b0ac7ac6114d77d25026bd08bab770a9673d
boualem <boualem.oussama@gmail.com>
2017-01-07 20:57:51 +0100

    front end : authentication panel + Register panel + home

---
+++ src/Home.java
@@ -0,0 +1,50 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+
+public class Home extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home frame = new Home();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Home() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 697, 382);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 11, 661, 265);
+		contentPane.add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Book ", null, panel, null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("See my places", null, panel_1, null);
+	}
+}
---
+++ src/Index.java
@@ -0,0 +1,84 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
+
+public class Index {
+
+	private JFrame frame;
+	private JTextField usernametf;
+	private JTextField passwordtf;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Index window = new Index();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Index() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel username = new JLabel("Username ");
+		username.setBounds(72, 79, 78, 14);
+		frame.getContentPane().add(username);
+		
+		JLabel password = new JLabel("Password");
+		password.setBounds(72, 133, 78, 14);
+		frame.getContentPane().add(password);
+		
+		usernametf = new JTextField();
+		usernametf.setBounds(209, 76, 86, 20);
+		frame.getContentPane().add(usernametf);
+		usernametf.setColumns(10);
+		
+		passwordtf = new JTextField();
+		passwordtf.setBounds(209, 130, 86, 20);
+		frame.getContentPane().add(passwordtf);
+		passwordtf.setColumns(10);
+		
+		JButton registerbtn = new JButton("Register");
+		registerbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("hello");
+				Register reg = new Register();
+				reg.setVisible(true);
+			}
+		});
+		registerbtn.setBounds(209, 195, 86, 23);
+		frame.getContentPane().add(registerbtn);
+		
+		JButton loginbtn = new JButton("Login");
+		loginbtn.setBounds(209, 161, 86, 23);
+		frame.getContentPane().add(loginbtn);
+	}
+	
+}
